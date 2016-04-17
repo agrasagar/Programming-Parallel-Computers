ba290bdf94be69e16f71b6b50fa2d5d618b8dc87
Bhattacharyya Agrasagar <bhattaa1@kiwi.org.aalto.fi>
2016-04-17 20:14:51 +0300

    mf1v1

--- mf1/mf.cc
+++ mf1/mf.cc
@@ -1,8 +1,53 @@
 #include "mf.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+
 
 void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
-    // FIXME
-    for (int i = 0; i < ny * nx; ++i) {
-        out[i] = in[i];
+    int size;
+    std::vector<float> medianFilter;
+    float median;
+    int x,y,window_y,window_x;
+   // medianFilter.reserve(hx*hy);   
+    for(y = 0; y< ny; y++){     //for traversing through y axis
+      for(x = 0; x< nx; x++){   //for traversing through x axis
+        medianFilter.clear();
+
+        for(window_y = y - hy; window_y<= y + hy ; window_y++){ //traversing window y axis
+
+          if(window_y >= 0 && window_y < ny){                     //check if window y exceeds given values
+
+            for(window_x = x - hx; window_x<= x + hx; window_x++){  //traversing window x axis
+
+              if(window_x >= 0 && window_x< nx){                  //check if window x exceeds given values
+
+                  medianFilter.push_back(in[window_x + window_y*nx]); //filling up median filter vector
+
+              }
+            }
+
+          }
+
+        }
+
+        size = medianFilter.size()/2;
+       // std::cout << "size is: " << size << '\n';
+	std::nth_element(medianFilter.begin(), medianFilter.begin() + (size), medianFilter.end());
+        median = medianFilter.at(size);
+	//std::cout << "first median value  is: " <<median<< '\n';
+        if ((medianFilter.size() % 2) == 0){
+          std::nth_element(medianFilter.begin(), medianFilter.begin()+(size-1), medianFilter.begin()+size);
+          median = (median + medianFilter[size-1])/2.0;
+	
+        }
+	//std::cout << "Final median value is: " << median << '\n';
+        out[x + y*nx] = median;
+
+      }
+
     }
 }
+
