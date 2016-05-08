d0f52fb5d98a9ad02f3dd467f2253a459955fddc
Bhattacharyya Agrasagar <bhattaa1@fluoriitti.org.aalto.fi>
2016-05-08 19:16:59 +0300

    soV1

--- so1/so.cc
+++ so1/so.cc
@@ -1,7 +1,36 @@
 #include "so.h"
+#include <math.h>
 #include <algorithm>
-
+#include <iostream>
+#include <omp.h>
 void psort(int n, data_t* data) {
     // FIXME: make this more efficient with parallelism
-    std::sort(data, data + n);
+	int npt=omp_get_max_threads();
+	int eachpt=n/npt;
+	int tmp=npt;
+	#pragma omp parallel for schedule(dynamic)
+	for(int i=0;i<npt;i++){
+		if(i==npt-1){
+			std::sort(data+(npt-1)*eachpt,data+n);
+		}
+		else{
+			std::sort(data+i*eachpt, data + (i+1)*eachpt);
+		}
+	}
+
+	while(tmp!=1){
+		#pragma omp parallel for schedule(dynamic)
+		for(int i=0;i<tmp;i+=2){
+			if((i+2)>=(tmp-1)){
+				std::inplace_merge(data+i*eachpt,data+(i+1)*eachpt,data+n);
+			}
+			else{
+				std::inplace_merge(data+i*eachpt,data+(i+1)*eachpt,data+(i+2)*eachpt);
+			}
+		}
+		eachpt=eachpt*2;
+		tmp=(tmp+1)*0.5;
+	}
+
+		
 }
