25a7e2781d1e9233fc1e946a255e22a2bb6d098a
Bhattacharyya Agrasagar <bhattaa1@kalkkuna.org.aalto.fi>
2016-05-08 21:46:05 +0300

    is1unfinished

--- is1/is.cc
+++ is1/is.cc
@@ -20,45 +20,62 @@ Result segment(int ny, int nx, const float* data) {
 	double4_t* sRect = double4_alloc((nx+1)*(ny+1));
 	double4_t totBack;
 	double4_t* col = double4_alloc(((nx+1)*(ny+1)));
-	travx = (nx + 1);
+	travx = nx+1;
 	totBack = double4_0;
-	std::cout<<std::endl;
-	for(i = 0; i< ny+1 ; ++i){
-		for(j = 0; j< nx+1; ++j){
-			col[i*travx+j] = double4_0;
-			if( j!=0){
+	std::cout<<"Colours:  "<<std::endl;
+	for(i = 0; i<= ny ; ++i){
+		for(j = 0; j<= nx; ++j){
+			//std::cout<<"check this one ("<<i<<","<<j<<")"<<"  "<<std::endl;
+			if(i == 0 || j ==0){
+				 col[i*travx+j] = double4_0;
+				 sRect[i*travx+j] = double4_0;
+			}
+			else{
+				col[i*travx+j] = double4_0;
 				for(c = 0; c<3; ++c){
-					col[i*travx+j][c] = data[c + 3 * (j-1) + 3 * nx * (i)];
+					col[i*travx+j][c] = data[c + 3 * (j-1) + 3 * nx * (i-1)];
 				}
 				totBack+=col[i*travx+j];
 			}
-			std::cout<<"("<<i<<","<<j<<")"<<col[i*travx+j][0]<<"  ";
+			sRect[i*travx+j] = double4_0;
+			std::cout<<"("<<j<<","<<i<<")"<<col[i*travx+j][0]<<"  ";
 		}	
 		std::cout<<std::endl;
 	}
-//	std::cout<<std::endl<<"SUM CALCULATION"<<std::endl;
-	//making sum from 0,0 point to x,y and saving them in sRectc
+	std::cout<<std::endl<<"col again"<<std::endl;
+;
+	for(i =0; i<=ny;i++){
+		for(j=0;j<=nx;j++){
+	std::cout<<j<<","<<i<<"    "<<col[i*travx+j][0]<<"  ";
+		}	
+		std::cout<<std::endl;
+	}
+	
+	std::cout<<std::endl<<"SUM CALCULATION sRect additions"<<std::endl;
+	//making sum from 0,0 point to x,y and saving them in sReact
 	/*{{{*/
-	for( i = 0; i< ny+1; ++i){
-		for( j= 1; j< nx+1; ++j){
-//			std::cout<<std::endl<<"("<<i<<","<<j<<")"<<std::endl;
+
+	for( i = 1; i<= ny; ++i){
+		for( j= 1; j<= nx; ++j){
+			std::cout<<std::endl<<"("<<j<<","<<i<<")"<<std::endl;
 			sRect[i*travx+j] = double4_0;
 			for( ii = i; ii>=0 ; --ii){
 				for(jj = j; jj>=0; --jj){
 					sRect[i*travx+j] += col[ii*travx + jj];
-//					std::cout<<std::endl<<"("<<ii<<","<<jj<<") "<<col[ii*travx + jj][0]<<" + ";
+					std::cout<<"("<<jj<<","<<ii<<") "<<col[ii*travx + jj][0]<<" + ";
 				}				
 			}
-//			std::cout<<std::endl;
+			std::cout<<std::endl;
 		}			
 	}
 
-//	std::cout<<std::endl<<"Check for total"<<std::endl;
-//	std::cout<<sRect[2*travx+1][0]<<"   tot is  "<<totBack[0]<<std::endl;
-	for( i = 0 ; i < ny+1 ; i++){
-		for(j=1 ; j < nx+1 ; j++){
-//			std::cout<<std::endl<<"value of srect 0 "<<sRect[i*travx + j][0]<<std::endl;
+	std::cout<<std::endl<<"Check for sRect pos and values"<<std::endl;
+	std::cout<<sRect[3*travx+1][0]<<"   tot is  "<<totBack[0]<<std::endl;
+	for( i = 0 ; i <=ny ; i++){
+		for(j=0 ; j <=nx ; j++){
+			std::cout<<"("<<j<<","<<i<<")"<<sRect[i*travx + j][0]<<"  ";
 		}
+		std::cout<<std::endl;
 	}
 	/*}}}*/	
 	double maxVal = 0;
@@ -66,73 +83,59 @@ Result segment(int ny, int nx, const float* data) {
 	double X;		//reciprocal of X
 	double Y;		//reciprocal of Y
 	int totSize = nx*ny;
-	int K,M;
+//	int K,M;
 	double4_t Hxy = double4_0;
 	//traversing through pixels with diff size of boxes
 	//making the box 2 consecutive for loops (0,0) constant
 	// i*j is the size of the boxes
-	for(i = 1; i<ny+1; ++i){
-		for(j=1; j<nx+1; ++j){
+	for(i = 1; i<=ny; ++i){
+		for(j=1; j<=nx; ++j){
 				
 				std::cout<<std::endl<<" i is "<<i<<" j is "<<j<<std::endl;
 				std::cout<<"--------------------------------------"<<std::endl;
 				ii = i*j;
-				//if( ii == totSize){
-				//	break;
-				//}
 				X = 1.0/double(ii);
 				jj = totSize - ii;
 				Y = 1.0/double(jj);
-				(void)X;
-				(void)Y;
+				
+				
 				//moving the box 2 consecutive for loops
-				for( k= 0; k<=ny-i; ++k){
+				for( k= 0; k<ny-i+1; ++k){
 					 double4_t Vxy = double4_0;
 					 double4_t Yxy = double4_0;
-					for(m = 0;m<=nx-j; ++m){
-						K = k+i-1;
-						M = m+j;
-						std::cout<<std::endl<<"value of start x y "<<k<<m<<std::endl;
-						std::cout<<std::endl<<"value of end x y "<<K<<M<<std::endl;
-						if( m==0 && k==0 ){
-							Vxy += sRect[K*travx + M];
-							std::cout<<std::endl<<"value of end In m&k==0 loop  x y "<<K<<M<<std::endl;
-						}
-						else if(m==0){
-							Vxy += sRect[K*travx + M]- sRect[(k-1)*travx + (M)];
-							std::cout<<std::endl<<"value of end In m==0 loop  x y "<<K<<M<<std::endl;
-							std::cout<<"value of end In  loop  K-1 M is  "<<k-1<<M<<std::endl;
-						}	
-						else if(k==0){
-							Vxy += sRect[K*travx + M]- sRect[(K)*travx + (m)];
-							std::cout<<std::endl<<"value of end In k==0 loop  x y "<<K<<M<<std::endl;
-                                                        std::cout<<"value of end In  loop  K m is  "<<K<<m<<std::endl;
-						}			
-						else{
-							Vxy += sRect[K*travx + M]- sRect[(K)*travx + (m)]- sRect[(k-1)*travx + (M)] + sRect[(k-1)*travx + (m)];
-						}
-		
-						Yxy += totBack-Vxy;
-			
-						Hxy[0] = pow(Vxy[0],2)*X + pow(Yxy[0],2)*Y;
-						Hxy[1] = pow(Vxy[1],2)*X + pow(Yxy[1],2)*Y;
-						Hxy[2] = pow(Vxy[1],2)*X + pow(Yxy[2],2)*Y;
-						tempMax = Hxy[0]+Hxy[1]+Hxy[2];
-						if(tempMax >= maxVal){
-							maxVal = tempMax;
-							std::cout<<std::endl<<"///////////////////////////////  x0 y0 x1 y1 are "<<k<<"  "<<m<<"  "<<M<<"  "<<K+1<<std::endl;
-							result.x0 = m;
-							result.y0 = k;
-							result.y1 = k+i-1;
-							result.x1 = M;
-							result.inner[0] = col[K*travx + M][0];
-							result.inner[1] = col[K*travx + M][1];
-							result.inner[2] = col[K*travx + M][2];
-							double val = 1.0/(jj);	
-							result.outer[0] = Yxy[0]*val;
-							result.outer[1] = Yxy[1]*val;
-							result.outer[2] = Yxy[2]*val;
-						}
+					for(m = 0;m<nx-j+1; ++m){
+							std::cout<<"value of x y are "<<m<<"  "<<k<<std::endl;
+							std::cout<<"values inside rect are "<<m+j<<"  "<<k+i<<std::endl;
+							//std::cout<<"value to check is "<<sRect[(k+i)*travx + (m+j)][0]<<std::endl;
+							//std::cout<<"value to check is "<<sRect[(k)*travx + (m+j)][0]<<std::endl;
+							if( k ==0 && m==0){
+								Vxy = sRect[(k+i)*travx + (m+j)];		
+							}
+							else if( m==0){
+								Vxy =sRect[(k+i)*travx + (m+j)]-sRect[(k)*travx + (m+j)];
+							}
+							else if( k==0){
+								Vxy = sRect[(k+i)*travx + (m+j)]-sRect[(k+i)*travx + (m)];
+							}	
+							else{
+								Vxy = sRect[(k+i)*travx + (m+j)] -sRect[(k+i)*travx + (m)]-sRect[(k)*travx + (m+j)]+sRect[(k)*travx + (m)];
+							}
+							Yxy = totBack - Vxy;
+							Hxy[0] = pow(Vxy[0],2)*X + pow(Yxy[0],2)*Y;
+							Hxy[1] = pow(Vxy[1],2)*X + pow(Yxy[1],2)*Y;
+							Hxy[0] = pow(Vxy[2],2)*X + pow(Yxy[2],2)*Y;		
+							tempMax = Hxy[0] + Hxy[1] + Hxy[2];
+							if(tempMax >= maxVal){
+								std::cout<<std::endl<<"y0 x0 y1 x1 are "<<k<<"  "<<m<<"  "<<k+i<<"  "<<m+j<<std::endl;
+								maxVal = tempMax;
+								result.x0 = m;
+								result.y0 = k;
+								result.x1 = m+j;
+								result.y1 = k+i;
+								result.inner[0] = col[(k+i)*travx + (m+j)][0];
+								result.inner[1] = col[(k+i)*travx + (m+j)][1];
+								result.inner[2] = col[(k+i)*travx + (m+j)][2];
+							}				
 //							std::cout<<std::endl<<"y0 x0 y1 x1 are "<<result.y0<<"  "<<result.x0<<"  "<<result.y1<<"  "<<result.x1<<std::endl;
 //							std::cout<<std::endl<<"colors of inner are  "<<result.inner[0]<<result.inner[1]<<result.inner[2]<<std::endl;
 						}
