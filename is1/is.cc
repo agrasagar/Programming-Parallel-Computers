8d99344a5d00840d5618360c819735424591599d
Bhattacharyya Agrasagar <bhattaa1@iibis.org.aalto.fi>
2016-05-17 23:14:14 +0300

    refactored sum from 0,0 of each pixel

--- is1/is.cc
+++ is1/is.cc
@@ -32,70 +32,58 @@ Result segment(int ny, int nx, const float* data) {
 			sRect[i*travx+j] = double4_0;
 		}	
 	}
-
-	for(i =0; i<=ny;i++){
-		for(j=0;j<=nx;j++){
-		}	
+	
+	for( i = 1; i<= ny; ++i){
+		for( j = 1; j<= nx ; ++j){
+			sRect[i*travx + j] += col[(i)*travx + (j)] + sRect[(i-1)*travx + (j)] + sRect[(i)*travx + (j-1)] - sRect[(i-1)*travx + (j-1)];
+		}
 	}
 	
-	//making sum from 0,0 point to x,y and saving them in sReact
-	/*{{{*/
 
-	for( i = 1; i<= ny; ++i){
-		for( j= 1; j<= nx; ++j){
-			sRect[i*travx+j] = double4_0;
-			for( ii = i; ii>=0 ; --ii){
-				for(jj = j; jj>=0; --jj){
-					sRect[i*travx+j] += col[ii*travx + jj];
-				}				
-			}
-		}			
-	}
-
-	 #pragma omp parallel
-	{ 
-	/*}}}*/	
+	(void)ii;
+	(void)jj;
+	
 	double maxVal = 0;
-	double tempMax = 0;
-	double X;		//reciprocal of X
-	double Y;		//reciprocal of Y
 	int totSize = nx*ny;
-	double4_t Hxy = double4_0;
+	#pragma omp parallel
+	{
+	double tempMax = 0;
 	//traversing through pixels with diff size of boxes
 	//making the box 2 consecutive for loops (0,0) constant
-	 #pragma omp for schedule(static,1)
 	for(i = 1; i<=ny; ++i){
 		for(j=1; j<=nx; ++j){
 				
-				ii = i*j;
-				X = 1.0/double(ii);
-				jj = totSize - ii;
-				Y = 1.0/double(jj);
-				
+				int totS = i*j;
+				double X = 1.0/double(totS);
+				int  totR = totSize - totS;
+				double Y = 1.0/double(totR);
+				int condk = ny-i+1;
+				int condm = nx-j+1;	
 				
 				//moving the box 2 consecutive for loops
-				for( k= 0; k<ny-i+1; ++k){
-					 double4_t Vxy = double4_0;
-					 double4_t Yxy = double4_0;
-					for(m = 0;m<nx-j+1; ++m){
-//							if( k ==0 && m==0){
-//								Vxy = sRect[(k+i)*travx + (m+j)];		
-//							}
-//							else if( m==0){
-//								Vxy =sRect[(k+i)*travx + (m+j)]-sRect[(k)*travx + (m+j)];
-//							}
-//							else if( k==0){
-//								Vxy = sRect[(k+i)*travx + (m+j)]-sRect[(k+i)*travx + (m)];
-//							}	
-//							else{
+				for( k= 0; k<condk; ++k){
+                                        double4_t Hxy = double4_0;
+                                        double4_t Vxy = double4_0;
+                                        double4_t Yxy = double4_0;
+					for(m = 0;m<condm; ++m){
+							
+							if( k ==0 && m==0){
+								Vxy = sRect[(k+i)*travx + (m+j)];		
+							}
+							else if( m==0){
+								Vxy =sRect[(k+i)*travx + (m+j)]-sRect[(k)*travx + (m+j)];
+							}
+							else if( k==0){
+								Vxy = sRect[(k+i)*travx + (m+j)]-sRect[(k+i)*travx + (m)];
+							}	
+							else{
 							Vxy = sRect[(k+i)*travx + (m+j)] -sRect[(k+i)*travx + (m)]-sRect[(k)*travx + (m+j)]+sRect[(k)*travx + (m)];
-//							}
+							}
 							Yxy = totBack - Vxy;
 							Hxy[0] = pow(Vxy[0],2)*X + pow(Yxy[0],2)*Y;
 							Hxy[1] = pow(Vxy[1],2)*X + pow(Yxy[1],2)*Y;
 							Hxy[2] = pow(Vxy[2],2)*X + pow(Yxy[2],2)*Y;		
 							tempMax = Hxy[0] + Hxy[1] + Hxy[2];
-							#pragma omp critical
 							if(tempMax >= maxVal){
 								maxVal = tempMax;
 								result.x0 = m;
